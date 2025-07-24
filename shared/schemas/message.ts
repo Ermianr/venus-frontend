e2e80feb0f62f950d09f2c87ca6e4203710b5cb0
Kevin Garcia <ermianrazor@gmail.com>
2025-07-23 21:53:20 -0500

    feat: se agrega soporte para mensajes tipo ping

--- shared/schemas/message.ts
+++ shared/schemas/message.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
 export const MessageFrontSchema = z.object({
-    type: z.enum(["text"]),
+    type: z.enum(["text", "ping"]),
     content: z.string().min(1).max(1000),
 });
