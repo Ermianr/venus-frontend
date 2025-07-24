0a681a25a8af8d9bc51986d352effd93bfbc7993
Kevin Garcia <ermianrazor@gmail.com>
2025-07-23 20:57:00 -0500

    feat: se agrega validación de mensajes para reforzar la seguridad

--- server/routes/ws/chat.ts
+++ server/routes/ws/chat.ts
@@ -1,27 +1,32 @@
 import type { User } from "#auth-utils";
-import type { MessageFront } from "~~/shared/types/message";
+import { MessageFrontSchema } from "#shared/schemas/message";
 
 export default defineWebSocketHandler({
     async upgrade(request) {
         await requireUserSession(request);
     },
     async open(peer) {
-        const { user } = await getUserSession(peer);
-        peer.context.user = user;
+        const { user: currentUser } = await getUserSession(peer);
+        peer.context.user = currentUser;
         peer.subscribe("global");
     },
     async message(peer, msg) {
-        const user = peer.context.user as User;
-        const message: MessageFront = msg.json();
-        await $fetch("/api/message", {
-            method: "POST",
-            body: {
-                content: message.content,
-                userId: user.id,
-                room: "global",
-            },
-        });
-        peer.send(`[${user.username}]: ${message.content}`);
-        peer.publish("global", `[${user.username}]: ${message.content}`);
+        const currentUser = peer.context.user as User;
+        const messageReceived = MessageFrontSchema.safeParse(msg.json());
+
+        if (!messageReceived.success) {
+            return;
+        } else {
+            await $fetch("/api/message", {
+                method: "POST",
+                body: {
+                    content: messageReceived.data.content,
+                    userId: currentUser.id,
+                    room: "global",
+                },
+            });
+            peer.send(`[${currentUser.username}]: ${messageReceived.data.content}`);
+            peer.publish("global", `[${currentUser.username}]: ${messageReceived.data.content}`);
+        }
     },
 });
