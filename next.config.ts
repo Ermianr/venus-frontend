b144c2e16c23c01de05e8e35d5c889b16120d59f
Kevin Garcia <ermianrazor@gmail.com>
2025-11-03 22:18:05 -0500

    Corrección para evitar problemas con la variable de entorno de la API

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   rewrites: async () => {
+    const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+    if (!backendURL) {
+      throw new Error(
+        "la variable de entorno NEXT_PUBLIC_BACKEND_URL no esta definida.",
+      );
+    }
     return [
       {
         source: "/api/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/:path*`,
+        destination: `${backendURL}/api/auth/:path*`,
       },
     ];
   },
